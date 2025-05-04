73f1058c14ecb0d78931ac9dc5e98a0aab2d77da
Milad Coder <miladcoder1390@gmail.com>
2025-05-04 21:05:50 +0330

    Config photo gallery

--- src/client/components/home/gallery/Gallery.jsx
+++ src/client/components/home/gallery/Gallery.jsx
@@ -3,14 +3,15 @@ import { Box } from '@mui/material';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 import ImageListItemBar from '@mui/material/ImageListItemBar';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
+import axios from 'axios';
 
 export default function Gallery() {
 
   const [open, setOpen] = React.useState(false);
   const [selectedSchool, setSelectedSchool] = React.useState(null);
+  const [schools, setSchools] = React.useState([])
 
   const handleOpen = (school) => {
     setOpen(true);
@@ -24,128 +25,65 @@ export default function Gallery() {
   }
 
 
-  const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 400,
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
-    boxShadow: 24,
-    p: 4,
-  };
+
+  React.useEffect(() => {
+    axios.get('http://localhost:5000/api/school/all').then(res => {
+      console.log("school", res.data)
+      setSchools(res.data.schools)
+      
+    }).catch(e => {
+      console.log(e);
+    })
+
+
+  }, [])
 
   return (
     <Box>
 
       <ImageList sx={{ width: '100%', height: 'auto' }}>
-        {itemData.map((item) => (
-          <ImageListItem key={item.img}>
+        {schools.map((school) => (
+          <ImageListItem key={school.school_image}>
             <img
-              srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
-              src={`${item.img}?w=248&fit=crop&auto=format`}
-              alt={item.title}
+              srcSet={`./images/uploaded/school/${school.school_image}?w=248&fit=crop&auto=format&dpr=2 2x`}
+              src={`./images/uploaded/school/${school.school_image}?w=248&fit=crop&auto=format`}
+              alt={school.school_name}
               loading="lazy"
-              onClick={() => { handleOpen(item) }}
+              onClick={() => { handleOpen(school) }}
             />
             <ImageListItemBar
-              title={item.title}
-              subtitle={<span>by: {item.author}</span>}
-              position="below"
+              title={school.school_name}
             />
           </ImageListItem>
         ))}
       </ImageList>
 
-      <div>
-        <Button onClick={handleOpen}>Open modal</Button>
+
         <Modal
           open={open}
           onClose={handleClose}
           aria-labelledby="modal-modal-title"
           aria-describedby="modal-modal-description"
-        >
-          <Box sx={style}>
-            <Typography id="modal-modal-title" variant="h6" component="h2">
-              Text in a modal
-            </Typography>
-            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-            </Typography>
+          >
+          <Box component={'div'} sx={{position: 'absolute', top: '50%', left: '50%',
+            transform:'translate(-50%, -50%)',
+            background: '#fff',
+            border: 'none',
+            outline: 'none',
+            padding: '10px',
+
+          }}>
+            <Typography>{selectedSchool && selectedSchool.school_name}</Typography>
             <img
-              srcSet={`${selectedSchool && selectedSchool.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
-              src={`${selectedSchool && selectedSchool.img}?w=248&fit=crop&auto=format`}
+              // srcSet={`/images/uploaded/school/${selectedSchool && selectedSchool.img}`}
+              src={selectedSchool && `/images/uploaded/school/${selectedSchool.school_image}`}
+              style={{maxHeight:"80vh"}}
               alt={"alt"}
             />
           </Box>
         </Modal>
-      </div>
 
 
     </Box>
   );
 }
-
-const itemData = [
-  {
-    img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
-    title: 'Breakfast',
-    author: '@bkristastucchio',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
-    title: 'Burger',
-    author: '@rollelflex_graphy726',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
-    title: 'Camera',
-    author: '@helloimnik',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
-    title: 'Coffee',
-    author: '@nolanissac',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
-    title: 'Hats',
-    author: '@hjrc33',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
-    title: 'Honey',
-    author: '@arwinneil',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
-    title: 'Basketball',
-    author: '@tjdragotta',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
-    title: 'Fern',
-    author: '@katie_wasserman',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
-    title: 'Mushrooms',
-    author: '@silverdalex',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
-    title: 'Tomato basil',
-    author: '@shelleypauls',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
-    title: 'Sea star',
-    author: '@peterlaster',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
-    title: 'Bike',
-    author: '@southside_customs',
-  },
-];
