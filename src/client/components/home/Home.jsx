c43c715525672c72a36abfb963ab20d3779095a5
Milad Coder <miladcoder1390@gmail.com>
2025-05-04 22:17:02 +0330

    Add slider

--- src/client/components/home/Home.jsx
+++ src/client/components/home/Home.jsx
@@ -1,10 +1,11 @@
+import Carousel from "./carousel/Carousel";
 import Gallery from "./gallery/Gallery";
 
 export default function Home() {
 
     return (
         <>
-            <h1>Home</h1>
+            <Carousel />
             <Gallery />
         </>
     )
